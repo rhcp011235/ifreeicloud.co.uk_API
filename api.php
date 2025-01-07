70ba81b7e3c09974dc75fd996264177aa3a6cfdb
John <john.b.hale@gmail.com>
2025-01-06 22:25:27 -0500

    add check balance and fix up

--- api.php
+++ api.php
@@ -1,16 +1,19 @@
 <?php
+$API_KEY = ''; // YOUR API KEY
+
 if (isset($_POST['imeis'])) {
     $imeis = preg_split('/\s+/', trim($_POST['imeis']));
     foreach ($imeis as $imei) {
         $myCheck = [
             "service" => 4, // FMI ON / OFF CHECK $0.1 each check
             "imei" => $imei,
-            "key" => "API_KEY" // YOUR API KEY
+            "key" => "$API_KEY" // YOUR API KEY
         ];
-        //echo $imei . "<br>";
         check_imei($myCheck);
     }
 }
+// check balance of the API KEY
+check_balance();
 
 function check_imei($myCheck)
 {
@@ -29,9 +32,31 @@ function check_imei($myCheck)
         echo "Error: $myResult->error";
     } else {
         echo $myResult->response;
-        //echo "<hr><pre>" . print_r($myResult->object, true) . "</pre><hr>"; // TEST ONLY
-        // Here you can access specific info!
-        // echo $myResult->object->model;
+    }
+}
+
+function check_balance()
+{
+    $myCheck = [
+        "key" => "$API_KEY", // YOUR API KEY
+        "accountinfo" => "balance"
+];
+
+$ch = curl_init("https://api.ifreeicloud.co.uk");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $myCheck);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+$myResult = json_decode(curl_exec($ch));
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+if ($httpcode != 200) {
+    echo "Error: HTTP Code $httpcode";
+} elseif ($myResult->success !== true) {
+    echo "Error: $myResult->error";
+} else {
+    echo $myResult->response; // Human-readable
     }
 }
 ?>
+
