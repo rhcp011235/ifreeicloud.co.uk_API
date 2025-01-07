98c98a3221445d648a0f89464ee0e3c8a554f493
John <john.b.hale@gmail.com>
2025-01-06 22:13:04 -0500

    test code

--- api.php
+++ api.php
@@ -1,18 +1,16 @@
 <?php
-//var_dump($_POST);
 if (isset($_POST['imeis'])) {
     $imeis = preg_split('/\s+/', trim($_POST['imeis']));
     foreach ($imeis as $imei) {
-        $myCheck["imei"] = $imei;
+        $myCheck = [
+            "service" => 4, // FMI ON / OFF CHECK $0.1 each check
+            "imei" => $imei,
+            "key" => "API_KEY" // YOUR API KEY
+        ];
         echo $imei . "<br>";
-        //check_imei($myCheck);
+        check_imei($myCheck);
     }
 }
-
-$myCheck["service"] = 4; // FMI ON / OFF CHECK $0.1 each check
-$myCheck["imei"] = ""; // BLANK IMEI
-$myCheck["key"] = ""; // YOUR API KEY
-
 
 function check_imei($myCheck)
 {
@@ -24,5 +22,16 @@ function check_imei($myCheck)
     $myResult = json_decode(curl_exec($ch));
     $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
-}
 
+    if ($httpcode != 200) {
+        echo "Error: HTTP Code $httpcode";
+    } elseif ($myResult->success !== true) {
+        echo "Error: $myResult->error";
+    } else {
+        echo $myResult->response;
+        echo "<hr><pre>" . print_r($myResult->object, true) . "</pre><hr>"; // TEST ONLY
+        // Here you can access specific info!
+        // echo $myResult->object->model;
+    }
+}
+?>
